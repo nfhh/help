a6b0461c703c87db16e271f1d5280f36554a7f47
Stoa <542950749@qq.com>
2020-06-03 18:21:27 +0800

    v3

--- resources/views/admins/layouts/_g.blade.php
+++ resources/views/admins/layouts/_g.blade.php
@@ -3,9 +3,9 @@
     <ul>
         <li>变量之间用英文逗号<strong> , </strong>分隔；</li>
         <li>数字列表( ) 比如：(变量a,变量b)；</li>
-        <li>图片$$ 比如：http://xxx.com/a.jpg；</li>
         <li>方块列表[] 比如：[变量a,变量b]；</li>
         <li>段落{} 比如：{变量a,变量b}；</li>
         <li>加粗** 比如：*变量a,变量b*。</li>
+        <li>图片$$ 比如：http://xxx.com/a.jpg；</li>
     </ul>
 </div>
