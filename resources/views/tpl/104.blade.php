7e00bfb8c1ffad45f2872bc5ef807ab81add107d
Stoa <542950749@qq.com>
2020-06-30 14:46:04 +0800

    后台优化

--- resources/views/tpl/104.blade.php
+++ resources/views/tpl/104.blade.php
@@ -1,43 +1,45 @@
-<?php
-$str = '';
-foreach ($vars as $k => $var) {
-    if ($var[0] === '{' && $var[strlen($var) - 1] === '}') {
-        preg_match('/{(.*)}/', $var, $matches1);
-        $str .= '<p>';
-        foreach (explode('|', $matches1[1]) as $v) {
-            if ($v[0] === '*' || $v[strlen($v) - 1] === '*') {
-                $v = str_replace('*', '', $v);
-                $str .= '<b>' . $excel[$v][$lan] . '</b>';
-            } elseif ($v[strlen($v) - 1] === ']') {
-                $pattern = '/\[(.*)\]/';
-                preg_match($pattern, $v, $matches2);
-                $href = $matches2[1];
-                $text = preg_replace($pattern, '', $v);
-                $str .= '<a href="' . $href . '">' . $excel[$text][$lan] . '</a>';
-            } else {
-                $str .= $excel[$v][$lan];
+<div class="text-center">
+    <?php
+    $str = '';
+    foreach ($vars as $k => $var) {
+        if ($var[0] === '{' && $var[strlen($var) - 1] === '}') {
+            preg_match('/{(.*)}/', $var, $matches1);
+            $str .= '<p>';
+            foreach (explode('|', $matches1[1]) as $v) {
+                if ($v[0] === '*' || $v[strlen($v) - 1] === '*') {
+                    $v = str_replace('*', '', $v);
+                    $str .= '<b>' . $excel[$v][$lan] . '</b>';
+                } elseif ($v[strlen($v) - 1] === ']') {
+                    $pattern = '/\[(.*)\]/';
+                    preg_match($pattern, $v, $matches2);
+                    $href = $matches2[1];
+                    $text = preg_replace($pattern, '', $v);
+                    $str .= '<a href="' . $href . '">' . $excel[$text][$lan] . '</a>';
+                } else {
+                    $str .= $excel[$v][$lan];
+                }
             }
-        }
-        $str .= '</p>';
-    } else {
-        $str .= '<p>';
-        foreach (explode('|', $var) as $v) {
-            if ($v[0] === '*' || $v[strlen($v) - 1] === '*') {
-                $v = str_replace('*', '', $v);
-                $str .= '<b>' . $excel[$v][$lan] . '</b>';
-            } elseif ($v[strlen($v) - 1] === ']') {
-                $pattern = '/\[(.*)\]/';
-                preg_match($pattern, $v, $matches2);
-                $href = $matches2[1];
-                $text = preg_replace($pattern, '', $v);
-                $str .= '<a href="' . $href . '">' . $excel[$text][$lan] . '</a>';
-            } else {
-                $str .= $excel[$v][$lan];
+            $str .= '</p>';
+        } else {
+            $str .= '<p>';
+            foreach (explode('|', $var) as $v) {
+                if ($v[0] === '*' || $v[strlen($v) - 1] === '*') {
+                    $v = str_replace('*', '', $v);
+                    $str .= '<b>' . $excel[$v][$lan] . '</b>';
+                } elseif ($v[strlen($v) - 1] === ']') {
+                    $pattern = '/\[(.*)\]/';
+                    preg_match($pattern, $v, $matches2);
+                    $href = $matches2[1];
+                    $text = preg_replace($pattern, '', $v);
+                    $str .= '<a href="' . $href . '">' . $excel[$text][$lan] . '</a>';
+                } else {
+                    $str .= $excel[$v][$lan];
+                }
             }
+            $str .= '</p>';
         }
-        $str .= '</p>';
     }
-}
-echo $str;
-?>
+    echo $str;
+    ?>
+</div>
 
