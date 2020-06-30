a4c9a032c138612ff523e711d5fec51260730fc7
Stoa <542950749@qq.com>
2020-06-30 18:23:57 +0800

    后台优化

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,7 +15,7 @@
             </ol>
         </nav>
     </div>
-    <div class="container h-100">
+    <div class="container pt-5 h-100">
         <div class="row align-items-center h-100 py-2">
             <div class="col-md-8 bg-white mx-auto">
                 @php
