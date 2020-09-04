8533cd28b8900cb80f98200033d2cc142330ebea
zgj <542950749@qq.com>
2020-09-04 16:26:58 +0800

    fix

--- resources/views/downloads/show.blade.php
+++ resources/views/downloads/show.blade.php
@@ -5,6 +5,7 @@
         @include('partials.download_h')
         <product tourl="/download/packages"
                  start="{{ trans('help.start') }}"
+                 selected_type_id="{{ trans('help.tnas') }}"
                  product_tip1="{{ trans('help.product_tip1') }}"
                  product_tip2="{{ trans('help.product_tip2') }}"
                  product_tip3="{{ trans('help.product_tip3') }}"></product>
