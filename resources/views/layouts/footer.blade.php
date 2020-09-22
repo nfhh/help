70f50d74197adc4cafebd30e92d2f03a18d0151b
Stoa <542950749@qq.com>
2020-09-22 10:59:51 +0800

    'fx'

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,6 +10,6 @@
 @if(config('app.env') === 'production')
     <script>
         window.$crisp=[];window.CRISP_WEBSITE_ID="244ab43a-eb0e-48dd-bd52-f2d95a33a3c9";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-        $crisp.push(["do", "helpdesk:search"])
+        // $crisp.push(["do", "helpdesk:search"])
     </script>
 @endif
