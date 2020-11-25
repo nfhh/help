d73404601b640afa32e03e819cadeca46a08397e
Stoa <542950749@qq.com>
2020-11-25 17:22:44 +0800

    fix

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -10,7 +10,7 @@ class EmailController extends Controller
 {
     public function store(Request $request)
     {
-        if ($request->email === 'admin@qq.com') {
+        if ($request->email === 'admin@qq.com' || !$request->email) {
             return response()->json([
                 'code' => 0,
             ], 200);
