8eaa6b89ef6ba1f83b4694aa013c9c3830d6912c
Stoa <542950749@qq.com>
2021-03-01 11:43:05 +0800

    换banner

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,7 +1,7 @@
 <div id="carousel" class="carousel slide w-100">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="https://img.terra-master.com/media/banner_cnq.jpg" class="mx-auto d-block img-fluid"
+            <img src="https://img.terra-master.com/media/banner_q_cn.jpg" class="mx-auto d-block img-fluid"
                  alt="">
         </div>
     </div>
